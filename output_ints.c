fbeddc3231e8cc64240c8da5887f3010122009fd
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-22 14:37:28 -0700

    int betty

--- output_ints.c
+++ output_ints.c
@@ -1,5 +1,11 @@
 #include "main.h"
-                                     int output_ints(va_list arg)
+/**
+ * output_ints - d & i
+ * Description: integer convertion
+ * @arg: arguments
+ * Return: count of digits
+ */
+int output_ints(va_list arg)
 {
 	int len, i, num, print_nums;
 	char buffer[16];
@@ -9,18 +15,18 @@
 	len = 0;
 	if (num < 0)
 	{
-		my_putchar('-');                        print_nums++;
+		my_putchar('-');
+		print_nums++;
 		num = -num;
 	}
-	do
-	{
+	do {
 		buffer[len++] = num % 10 + '0';
 		num /= 10;
 	} while (num != 0);
 	for (i = len - 1; i >= 0; i--)
 	{
-        my_putchar(buffer[i]);
-        print_nums++;
+		my_putchar(buffer[i]);
+		print_nums++;
 	}
 	return (print_nums);
 }
