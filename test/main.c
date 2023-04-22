9d610fdf8def3f15bd1537457eb462e079534c9d
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-22 16:30:19 -0700

    Revert "Delete test directory"
    This reverts commit fb5cc9f4fd23af57516d676454e1475ba6f9577b.
    
    revert

---
+++ test/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "../main.h"                                                       /**                                 * main - Entry point                *                                   * Return: Always 0                  */
+
+int main(void)
+{
+printf("%%%%\n");
+_printf("%%%%\n");
+printf("this my schoool is hard  %s\n", "Holberton"); 
+
+_printf("this my schoool is hard  %s\n", "Holberton");
+
+_printf("this my schoool is hard  %c\n", 'H');
+
+printf("this my schoool is hard  %c\n", 'H');
+	return (0);
+}
