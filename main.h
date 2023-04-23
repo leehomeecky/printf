0c99470f09924f79660c1179b33aa76aa2824660
leehomeecky <meecky4christ@gmail.com>
2023-04-23 16:12:24 -0700

    main.h fix

--- main.h
+++ main.h
@@ -5,9 +5,9 @@
 #include <stdarg.h>
 
 /**
- * struct print_format - This struct will be used to filter which print
+ * struct print_formart - This struct will be used to filter which print
  * function for i,c,s,d,x,X,o,p,u
- * * @spec: % format specifier to use
+ * @spec: % format specifier to use
  * @selectprint: function pointer to select the correct printer
  */
 typedef struct print_formart
@@ -20,7 +20,7 @@ typedef struct print_formart
  * struct flag_function - a stucture to filter the approprate flag function
  *
  * @flag: the falg
- * @flag_opt - fuction for the flag operation
+ * @flag_opt: fuction for the flag operation
  */
 
 typedef struct flag_function
@@ -34,6 +34,6 @@ int my_putchar(const char c);
 int writeout(char ch);
 int output_alpha(const char *, va_list arg);
 int output_char(const char *, va_list arg);
-int _printf(const char *format, ...);int output_ints(va_list arg);
+int _printf(const char *format, ...);
 
 #endif
