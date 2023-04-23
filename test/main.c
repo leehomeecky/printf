532adcd47544a670826962a1d0a3df399dc9a8c1
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-23 05:12:15 -0700

    test for -1

--- test/main.c
+++ test/main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-#include "../main.h"                                               
-int main() {
-    char c = 'X';
-    char b = 'B';
+#include "../main"
 
-    _printf("Character: %.-43we+2c\n", c);
-_printf("Character: %.yachje%c\n", c, b);
 
-    _printf("Escape character: %c\n", '\n');
-    _printf("ASCII code: %c\n", 65);
 
-    _printf("Character: .weejc\n");
-    _printf("%829-+ #7s", "Character: .weejc\n");
+
+
     return 0;
 }
