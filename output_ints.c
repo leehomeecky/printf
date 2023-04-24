3d3a984e169c9528dd73e06db23aa4cd18ba5ffd
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 02:19:07 -0700

    integer buffer added

--- output_ints.c
+++ output_ints.c
@@ -10,7 +10,7 @@
 int output_ints(const char *s, va_list arg)
 {
 	int len, i, num, print_nums;
-	char buffer[32];
+	char result[19];
 	const char *t = s;
 
 	print_nums = 0;
@@ -22,18 +22,16 @@ int output_ints(const char *s, va_list arg)
 
 	if (num < 0)
 	{
-		my_putchar('-');
-		print_nums++;
+		print_nums += my_putchar('-');
 		num = -num;
 	}
 	do {
-		buffer[len++] = num % 10 + '0';
+		result[len++] = num % 10 + '0';
 		num /= 10;
 	} while (num != 0);
 	for (i = len - 1; i >= 0; i--)
 	{
-		my_putchar(buffer[i]);
-		print_nums++;
+		print_nums += my_putchar(result[i]);
 	}
 	while ((*t != 'd' && *(t - 1) != '%') || (*t != 'i' && *(t - 1) != '%'))
 		t++;
--- test/main.c
+++ test/main.c
@@ -17,6 +17,7 @@ len2 = printf("Let's try to printf a simple sentence.\n");
 _printf("Length:[%d, %i]\n", len, len);
 printf("Length:[%d, %i]\n", len2, len2);
 _printf("Negative:[%d]\n", -762534);
+_printf("test:[%d 899]\n", -762534);
 _printf("Character:[%c]\n", 'H');
 printf("Character:[%c]\n", 'H');
 _printf("String:[%s]\n", "I am a string !");
