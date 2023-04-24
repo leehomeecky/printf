8f9a3cbbd73cb4458ef815baa7cf3f2d6b5ab0d3
leehomeecky <meecky4christ@gmail.com>
2023-04-24 06:01:05 -0700

    ROT13 indenting

--- output_ROT13.c
+++ output_ROT13.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * output_ROT13 - a function that output strings in ROT13
  *
@@ -13,31 +12,30 @@
 int output_ROT13(const char *str, va_list arg)
 {
 
-int i, count_rot, j;
-char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-char *n = va_arg(arg, char *);
+	int i, count_rot, j;
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *n = va_arg(arg, char *);
 
-	/*(void)f; */
 	count_rot = 0;
 	if (!n)
 	{
 	return (0);
 	}
-while (*str != 'R')
+	while (*str != 'R')
 	str++;
 
-for (i = 0; n[i] != '\0'; i++)
-{
-for (j = 0; s1[j] != '\0'; j++)
-{
-if (n[i] == s1[j])
-{
-my_putchar(s2[j]);
-count_rot++;
-break;
-}
-}
-}
-return (count_rot);
+	for (i = 0; n[i] != '\0'; i++)
+	{
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+	if (n[i] == s1[j])
+	{
+	my_putchar(s2[j]);
+	count_rot++;
+	break;
+	}
+	}
+	}
+	return (count_rot);
 }
