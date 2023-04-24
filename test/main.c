cff35806db008371e34aefe1560425be5f70d322
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-23 22:15:44 -0700

    test case

--- test/main.c
+++ test/main.c
@@ -1,7 +1,25 @@
 #include "../main.h"
+#include <limits.h>
+#include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
+int len;
+int len2;
 
-	return (0);
+len = _printf("Let's try to printf a simple sentence.\n");
+len2 = printf("Let's try to printf a simple sentence.\n");
+_printf("Character:[%c]\n", 'H');
+printf("Character:[%c]\n", 'H');
+_printf("String:[%s]\n", "I am a string !");
+printf("String:[%s]\n", "I am a string !");
+len = _printf("Percent:[%%]\n");
+len2 = printf("Percent:[%%]\n");
+return (0);
 }
+
