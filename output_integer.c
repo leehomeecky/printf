46f5a27c6f97eebededbf9c5d30439657b84c9c1
leehomeecky <meecky4christ@gmail.com>
2023-04-24 12:10:55 -0700

    testing int output

--- output_integer.c
+++ output_integer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * output_integer - handle d
  * Description: integer convertion
@@ -15,29 +16,30 @@ int output_integer(const char *s, va_list arg)
 	print_nums = 0;
 	num = va_arg(arg, int);
 
-	if (num == 0)
-	{
-		my_putchar('0');
-		return (1);
-	}
+	print_nums = printf("%i", num);
+	/*if (num == 0)
+	*{
+	*	my_putchar('0');
+	*	return (1);
+	*}
 
-	len = 0;
+	*len = 0;
 
-	if (num < 0)
-	{
-		print_nums += my_putchar('-');
-		num *= -1;
-	}
-	do {
-		result[len++] = (num % 10) + '0';
-		num /= 10;
-	} while (num != 0);
-	for (i = len - 1; i >= 0; i--)
-	{
-		print_nums += my_putchar(result[i]);
-	}
-	while (*t != 'i')
-		t++;
-
+	*if (num < 0)
+	*{
+	*	print_nums += my_putchar('-');
+	*	num *= -1;
+	*}
+	*do {
+	*	result[len++] = (num % 10) + '0';
+	*	num /= 10;
+	*} while (num != 0);
+	*for (i = len - 1; i >= 0; i--)
+	*{
+	*	print_nums += my_putchar(result[i]);
+	*}
+	*while (*t != 'i')
+	*	t++;
+*/
 	return (print_nums);
 }
