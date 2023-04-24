132bced8deda64182cb778a619eb3a8b5d317473
leehomeecky <meecky4christ@gmail.com>
2023-04-24 04:41:11 -0700

    Rot13 add

---
+++ output_ROT13.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include "main.h"
+
+
+/**
+ * output_ROT13 - a function that output strings in ROT13
+ *
+ * @arg: variable function value
+ * @str: pointer to character to print
+ *
+ * Return: 0 if success
