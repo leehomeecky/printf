1a99eff02ef8f9a2a146ba8142768bb528560db0
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 23:26:17 -0700

    testing datatypes

--- output_decimal.c
+++ output_decimal.c
@@ -8,8 +8,8 @@
  */
 int output_decimal(const char *s, va_list arg)
 {
-	int a[16];
-	long int j = 1, n, m = 1000000000000000, sum = 0, counter = 0;
+	int a[17];
+	long int j = 1, n, m = 10000000000000000, sum = 0, counter = 0;
 
 	while (*s != 'd')
 		s++;
@@ -23,16 +23,16 @@ int output_decimal(const char *s, va_list arg)
 	}
 	a[0] = n / m;
 
-	for (; j < 16; j++)
+	for (; j < 17; j++)
 	{
 		m /= 10;
 		a[j] = (n / m) % 10;
 	}
 
-	for (j = 0; j < 16; j++)
+	for (j = 0; j < 17; j++)
 	{
 		sum += a[j];
-		if (sum != 0 || j == 16)
+		if (sum != 0 || j == 17)
 		{
 			my_putchar('0' + a[j]);
 			counter++;
