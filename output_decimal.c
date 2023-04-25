d08fd0533f4005d8c789c0a9e1387822627e2f2e
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 20:37:21 -0700

    modified unsigned int

--- output_decimal.c
+++ output_decimal.c
@@ -1,45 +1,46 @@
-
-
-
 #include "main.h"
-#include <limits.h>
 /**
  * output_decimal - handle d
  * Description: integer convertion
  * @arg: arguments
  * @s: ###########
- * Return: count of digits
+ * Return: c/ount of digits
  */
 int output_decimal(const char *s, va_list arg)
 {
-	int len, i, num, print_nums;
-	char result[1024];
-	const char *t = s;
-
+	int sum, num, j, print_nums;
+	int  result[1024];
+	int m; 
+	
+	m = 1000000000;
 	print_nums = 0;
+	j = 1;
+	(void)s;
 	num = va_arg(arg, int);
 
-	if (num == 0)
-	{
-		my_putchar('0');
-		return (1);
-	}
-	len = 0;
+
 	if (num < 0)
 	{
 		print_nums += my_putchar('-');
 		num *= -1;
 	}
-	do {
-		result[len++] = num % 10 + '0';
-		num /= 10;
-	} while (num != 0);
-	for (i = len - 1; i >= 0; i--)
+	result[0] = num / m;
+
+	for (; j < 10; j++)
 	{
-		print_nums += my_putchar(result[i]);
+		m /= 10;
+		result[j] = (num / m) % 10;
 	}
-	while (*t != 'd')
-		t++;
+
+	for (j = 0; j < 10; j++)
+	{
+		sum += result[j];
+		if (sum != 0 || j == 9)
+		{
+			my_putchar('0' + result[j]);
+			print_nums++;
+		}
+	}
 
 	return (print_nums);
 }
