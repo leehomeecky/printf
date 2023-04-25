bac31bda0bb20a9d7efe15b75f5b6ac8c9425077
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 21:43:00 -0700

    int modification

--- output_integer.c
+++ output_integer.c
@@ -13,6 +13,7 @@ int output_integer(const char *s, va_list arg)
 	signed  long int j = 1, m = 1000000000000000, n;
 
 	long int counter = 0, sum = 0;
+
 	n = va_arg(arg, int);
 	while (*s != 'i')
 		s++;
