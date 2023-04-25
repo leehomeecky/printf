ee1ef5d776de388a128e9f1841d1cd16efc62ffc
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-25 02:01:52 -0700

    changes on %S

--- output_bigSX.c
+++ output_bigSX.c
@@ -6,28 +6,30 @@
  */
 int output_bigSX(va_list X)
 {
-    unsigned int a[8];
-    unsigned int m = 268435456, n, sum = 0;
-    char diff;
-    int counter, i = 1;
-    
-    n = va_arg(X, unsigned int);
-    for (; i < 8; i++)
-    {
-        a[i] = (n / m) % 16;
-        m /= 16;
-    }
+	unsigned int a[8];
+	unsigned int j = 1, m = 268435456, n, sum = 0;
+	char diff;
+	int counter;
 
-    for (i = 0; i < 8; i++)
-    {
-        sum += a[i];
-        if (sum || i == 7)
-        {
-            diff = a[i] < 10 ? '0' : 'A' - 10;
-            my_putchar(a[i] + diff);
-            counter++;
-        }
-    }
-
-    return (counter);
+	n = va_arg(X, unsigned int);
+	diff = 'A' - ':';
+	a[0] = n / m;
+	for (; j < 8; j++)
+	{
+		m /= 16;
+		a[j] = (n / m) % 16;
+	}
+	for (j = 0; j < 8; j++)
+	{
+		sum += a[j];
+		if (sum || j == 7)
+		{
+			if (a[j] < 10)
+				my_putchar('0' + a[j]);
+			else
+				my_putchar('0' + diff + a[j]);
+			counter++;
+		}
+	}
+	return (counter);
 }
