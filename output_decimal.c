56ec92e65066413718aed7ce3b97942f84b02e4f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 21:51:21 -0700

    int modification

--- output_decimal.c
+++ output_decimal.c
@@ -11,7 +11,7 @@ int output_decimal(const char *s, va_list arg)
 	int a[16];
 	long int j = 1, n, m = 1000000000000000, sum = 0, counter = 0;
 
-	while (*s != '\0')
+	while (*s != 'd')
 		s++;
 
 	n = va_arg(arg, int);
