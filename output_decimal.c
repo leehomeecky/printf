13cb4659c524b55266d51c5655bdb8b34ceda2c1
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 21:34:40 -0700

    int modification

--- output_decimal.c
+++ output_decimal.c
@@ -8,8 +8,8 @@
  */
 int output_decimal(const char *s, va_list arg)
 {
-	int a[20];
-	long int j = 1, m = 10000000000000000000, n, sum = 0, counter = 0;
+	int a[16];
+	long int j = 1, n, m = 1000000000000000, sum = 0, counter = 0;
 
 	while (*s != '\0')
 		s++;
@@ -23,16 +23,16 @@ int output_decimal(const char *s, va_list arg)
 	}
 	a[0] = n / m;
 
-	for (; j < 20; j++)
+	for (; j < 16; j++)
 	{
 		m /= 10;
 		a[j] = (n / m) % 10;
 	}
 
-	for (j = 0; j < 20; j++)
+	for (j = 0; j < 16; j++)
 	{
 		sum += a[j];
-		if (sum != 0 || j == 20)
+		if (sum != 0 || j == 16)
 		{
 			my_putchar('0' + a[j]);
 			counter++;
