94f36daeeb048d8e0b7d361c30e767c3d23bfcdb
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-24 22:14:27 -0700

    int array added

--- output_decimal.c
+++ output_decimal.c
@@ -8,13 +8,13 @@
  */
 int output_decimal(const char *s, va_list arg)
 {
-	int a[16];
-	long int j = 1, n, m = 1000000000000000, sum = 0, counter = 0;
+	int a[17];
+	long int j = 1, n, m = 100000000000000000, sum = 0, counter = 0;
 
 	while (*s != 'd')
 		s++;
 
-	n = va_arg(arg, long int);
+	n = va_arg(arg, int);
 	if (n < 0)
 	{
 		n *= -1;
@@ -23,13 +23,13 @@ int output_decimal(const char *s, va_list arg)
 	}
 	a[0] = n / m;
 
-	for (; j < 16; j++)
+	for (; j < 17; j++)
 	{
 		m /= 10;
 		a[j] = (n / m) % 10;
 	}
 
-	for (j = 0; j < 16; j++)
+	for (j = 0; j < 17; j++)
 	{
 		sum += a[j];
 		if (sum != 0 || j == 16)
