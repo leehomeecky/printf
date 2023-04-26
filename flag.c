a21895a31b47da45580bfeee97fabce20935dd24
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-04-26 17:29:38 +0100

    Update flag.c

--- flag.c
+++ flag.c
@@ -46,9 +46,9 @@ int flag_hash(const char **str)
 
 int flag_space(const char **s, long int val)
 {
-	int i = 0, val = 1;
+	int i = 0;
 
-	for (; (**s == ' '); (*s)++)
+	for (; (**s == ' ') || (**s == '#'); (*s)++)
 		i = 1;
 	if (val < 0 || (**s != 'i' && **s != 'l' && **s != 'd' && **s != 'h'))
 		i = 0;
