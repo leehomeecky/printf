e5449bb4afa7f832c1ccc556ce4443abd03b30f3
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-04-26 17:40:24 +0100

    Update output_integer.c

--- output_integer.c
+++ output_integer.c
@@ -1,5 +1,56 @@
 #include "main.h"
 /**
+ * iflag_handler - handles flag for int
+ *
+ * @str: pointer to string
+ * @len: lenght of str to print
+ * @value: value to be printed
+ *
+ * Return: number of printed value
+ */
+
+int iflag_handler(const char **str, int *arr, int len_arr, long int value)
+{
+	int count = 0, zero, minus = 0, width = 0, dot = precision(*str, 'i');
+	int len = count_array(arr, len_arr), space = flag_space(str, value), plus = flag_plus(str);
+	
+	(dot < 0 && **str == '0') ? (zero = justify(str, 'i')) : (zero = 0);
+	(**str == '-') ? (minus = justify(str, 'i')) : (width = justify(str, 'i'));
+	(value >= 0) ? (zero -= plus) : (zero--);
+	if (dot >= 0)
+	{
+	(dot > len) ? (dot -= len) : (dot = 0);
+	width -= (dot + len);
+	minus -= (dot + len);
+	}
+	else
+	{
+	(zero > len) ? (zero -= len) : (zero = 0);
+	width -= (zero + len);
+	minus -= (zero + len);
+	}
+	count += print_recus(space, ' ');
+	if (value >= 0)
+	{
+	count += print_recus((width -= plus), ' ');
+	count += print_recus(plus, '+');
+	minus--;
+	}
+	else
+	{
+	count += print_recus((width -= 1), ' ');
+	count += print_recus(1, '-');
+	minus--;
+	}
+	count += print_recus(dot, '0');
+	count += print_recus(zero, '0');
+	count += print_array_val(arr, len_arr);
+	count += print_recus(minus, ' ');
+	return (count);
+}
+
+
+/**
  * output_integer - handle d
  * Description: integer convertion
  * @arg: arguments
@@ -9,7 +60,7 @@
 int output_integer(const char *s, va_list arg)
 {
 	int a[18], h;
-	long int j = 1, n, m = 100000000000000000;
+	long int j = 1, n, value, m = 100000000000000000;
 
 	int counter = 0, sum = 0;
 
@@ -17,14 +68,9 @@ int output_integer(const char *s, va_list arg)
 	(h == 2) ? (n = va_arg(arg, long int)) : (n = va_arg(arg, int));
 	if (h == 1)
 		n = (short) n;
-	if (n == 0)
-		counter += my_putchar('0');
+	value = n;
 	if (n < 0)
-	{
 		n *= -1;
-		my_putchar('-');
-		counter++;
-	}
 	a[0] = n / m;
 
 	for (; j < 18; j++)
@@ -32,15 +78,8 @@ int output_integer(const char *s, va_list arg)
 		m /= 10;
 		a[j] = (n / m) % 10;
 	}
-
-	for (j = 0; j < 18; j++)
-	{
-		sum += a[j];
-		if (sum != 0 || j == 18)
-		{
-			my_putchar('0' + a[j]);
-			counter++;
-		}
-	}
+	counter += iflag_handler(&s, a, 18, value);
+	if (n == 0)
+		counter += my_putchar('0');
 	return (counter);
 }
